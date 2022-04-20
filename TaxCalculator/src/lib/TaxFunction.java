780dbaa29231b4f1dee993cebda50b9770a52a84
Muhamad Dwiki Riswanda <m.dwikiriswanda@gmail.com>
2022-04-20 22:43:54 +0700

    Mengganti Comment
    Ditemukan bad smells yaitu comment pada line ke 13 dimana yang awalnya "ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga." menjadi -> "ditambah sebesar Rp 1.500.000 per anak sampai anak ketiga." Hal ini sesuai dengan kodingan pada line ke 31 yaitu "... + (numberOfChildren * 1500000))));"

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -10,7 +10,7 @@ public class TaxFunction {
 	 * 
 	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
 	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
+	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 1.500.000 per anak sampai anak ketiga.
 	 * 
 	 */
 	
